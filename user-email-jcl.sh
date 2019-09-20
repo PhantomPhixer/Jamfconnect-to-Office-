a4b45737bd542e529313fceb2ca9f72c48eddd29
Mark Lamont <lamont853@gmail.com>
2019-09-20 11:40:07 +0100

    update

--- user-email-jcl.sh
+++ user-email-jcl.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
-#*################################################################################
-#! best viewed/edited in *Visual Studio Code* with *Better Comments* extension added
-#* This script finds the value in the jamf connect verify plist create dby jamf connect logon
-#* This is their upn or email address required for one click O365 setup
-#* This is then used to populate the users email address in jamf
-#! needs an EA to read in the receipt which can then be use in a smart group to scope the office one click config profile
-#! EA code is shown below prefixed by #? characters.
-#? #!/bin/bash
+#################################################################################
+# This script finds the value in the jamf connect verify plist create dby jamf connect logon
+# This is their upn or email address required for one click O365 setup
+# This is then used to populate the users email address in jamf
+# needs an EA to read in the receipt which can then be use in a smart group to scope the office one click config profile
 
-#? #EA to record office profile deployment ready
-#? result="no"
-
-#? if [ -f /Library/Management/receipts/deploy-office-profile ]; then
-#? result=yes
-#? fi
-
-#? echo "<result>$result</result>"
 #*################################################################################
 
 
